49ff41b9ac16c78ccf822b266288d5b6bf57d4da
DoChuan13 <chuando.9213@gmail.com>
2023-06-13 16:32:56 +0700

    Heroku Deploy Demo

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,83 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.request.SignInForm;
+import com.example.demo.dto.request.SignUpForm;
+import com.example.demo.dto.response.JwtResponse;
+import com.example.demo.dto.response.ResponMessage;
+import com.example.demo.model.Role;
+import com.example.demo.model.RoleName;
+import com.example.demo.model.User;
+import com.example.demo.security.jwt.JwtProvider;
+import com.example.demo.security.userprincal.UserPrinciple;
+import com.example.demo.service.impl.RoleServiceImpl;
+import com.example.demo.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
+
+@RequestMapping("/api/auth")
+@RestController
+@CrossOrigin(origins = "*")
+public class AuthController {
+    @Autowired
+    UserServiceImpl userService;
+    @Autowired
+    RoleServiceImpl roleService;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    JwtProvider jwtProvider;
+    @PostMapping("/signup")
+    public ResponseEntity<?> register(@Valid @RequestBody SignUpForm signUpForm){
+        if(userService.existsByUsername(signUpForm.getUsername())){
+            return new ResponseEntity<>(new ResponMessage("nouser"), HttpStatus.OK);
+        }
+        if(userService.existsByEmail(signUpForm.getEmail())){
+            return new ResponseEntity<>(new ResponMessage("noemail"), HttpStatus.OK);
+        }
+        User user = new User(signUpForm.getName(), signUpForm.getUsername(), signUpForm.getEmail(),passwordEncoder.encode(signUpForm.getPassword()));
+        Set<String> strRoles = signUpForm.getRoles();
+        Set<Role> roles = new HashSet<>();
+        strRoles.forEach(role ->{
+            switch (role){
+                case "admin":
+                    Role adminRole = roleService.findByName(RoleName.ADMIN).orElseThrow(
+                            ()-> new RuntimeException("Role not found")
+                    );
+                    roles.add(adminRole);
+                    break;
+                case "pm":
+                    Role pmRole = roleService.findByName(RoleName.PM).orElseThrow( ()-> new RuntimeException("Role not found"));
+                    roles.add(pmRole);
+                    break;
+                default:
+                    Role userRole = roleService.findByName(RoleName.USER).orElseThrow( ()-> new RuntimeException("Role not found"));
+                    roles.add(userRole);
+            }
+        });
+        user.setRoles(roles);
+        userService.save(user);
+        return new ResponseEntity<>(new ResponMessage("yes"), HttpStatus.OK);
+    }
+    @PostMapping("/signin")
+    public ResponseEntity<?> login(@Valid @RequestBody SignInForm signInForm){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(signInForm.getUsername(), signInForm.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtProvider.createToken(authentication);
+        UserPrinciple userPrinciple = (UserPrinciple) authentication.getPrincipal();
+        return ResponseEntity.ok(new JwtResponse(token, userPrinciple.getName(),userPrinciple.getAvatar(), userPrinciple.getAuthorities()));
+    }
+}
---
+++ src/main/java/com/example/demo/dto/response/JwtResponse.java
@@ -0,0 +1,80 @@
+package com.example.demo.dto.response;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtResponse {
+    private Long id;
+    private String token;
+    private String type = "Bearer";
+    private String name;
+    private String avatar;
+    private Collection<? extends GrantedAuthority> roles;
+
+    public JwtResponse() {
+    }
+
+    public JwtResponse(Long id, String token, String type, String name, Collection<? extends GrantedAuthority> roles) {
+        this.id = id;
+        this.token = token;
+        this.type = type;
+        this.name = name;
+        this.roles = roles;
+    }
+
+    public JwtResponse(String token, String name, String avatar, Collection<? extends GrantedAuthority> authorities) {
+        this.token = token;
+        this.name = name;
+        this.avatar = avatar;
+        this.roles = authorities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<? extends GrantedAuthority> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<? extends GrantedAuthority> roles) {
+        this.roles = roles;
+    }
+}
